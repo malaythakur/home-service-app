7e4e33c8409abfa96e754f561b02b2160a1e6c7f
malaythakur <malaythakur13@gmail.com>
2024-08-28 11:21:15 +0530

    Initial Commit (Project Setup)

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import styles from "./page.module.css";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Trustique Assist</h2>
+      <Button className="bg-red-600"> Subscribe!!! </Button>
+    </div>
+  );
+}
