757ec479eec0c7ac1ad4c04ac47393aa8a13aa82
malaythakur <malaythakur13@gmail.com>
2024-08-31 16:52:21 +0530

    Included Header component in the layout for a consistent navigation structure.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="mx-6 md:mx-16">
+          <Header/>
+          {children}
+        </div>
+
+      </body>
     </html>
   );
 }
