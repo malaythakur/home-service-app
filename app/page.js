5dcbad4c588e94a5a03ee6b81df90ac189230ea0
malaythakur <malaythakur13@gmail.com>
2024-09-01 02:10:19 +0530

    feat(page): add Hero component to homepage layout

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      
+      <Hero/>
     </div>
   );
 }
