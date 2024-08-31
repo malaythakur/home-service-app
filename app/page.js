cdee6927e035eac248b47929f8aee269cdfe1f77
malaythakur <malaythakur13@gmail.com>
2024-08-31 16:54:53 +0530

    feat(page): add initial setup with Button and Image components

--- app/page.js
+++ app/page.js
@@ -2,12 +2,10 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import styles from "./page.module.css";
 
-
 export default function Home() {
   return (
     <div>
-      <h2>Trustique Assist</h2>
-      <Button className="bg-red-600"> Subscribe!!! </Button>
+      
     </div>
   );
 }
