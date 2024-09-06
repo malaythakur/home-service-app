2fddd775ec11a43e22f16858fa6194ea17b37ba2
malaythakur <malaythakur13@gmail.com>
2024-09-06 21:16:38 +0530

    Add category fetching and display in Home component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,28 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./_components/Hero";
-
+import CategoryList from "./_components/CategoryList";
+import GlobalApi from "./_services/GlobalApi";
+import { useEffect, useState } from "react";
 export default function Home() {
+
+  const [categoryList,setCategoryList]=useState([]);
+
+  useEffect(()=>{
+    getCategoryList();
+  },[])
+
+  const getCategoryList=()=>{
+    GlobalApi.getCategory().then(resp=>{
+      setCategoryList(resp.categories);
+    })
+  }
   return (
     <div>
       <Hero/>
+      <CategoryList categoryList={categoryList} />
     </div>
   );
 }
