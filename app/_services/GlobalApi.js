0282abbc5b8152365de4933a6e8c21367c84975d
malaythakur <malaythakur13@gmail.com>
2024-09-09 09:48:12 +0530

    Added getAllBusinessList function to fetch business list data with GraphQL

--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -18,9 +18,33 @@ const getCategory=async()=>{
         }
         `
         const result = await request(MASTER_URL,query)
-    return result
+        return result
+}
+
+const getAllBusinessList=async()=>{
+    const query=gql`
+    query BusinessList {
+        businessLists {
+            about
+            address
+            category {
+            name
+            }
+            contactPerson
+            email
+            images {
+            url
+            }
+            id
+            name
+        }
+    }
+    `
+    const result= await request(MASTER_URL,query)
+    return result;
 }
 
 export default{
-    getCategory
+    getCategory,
+    getAllBusinessList
 }
