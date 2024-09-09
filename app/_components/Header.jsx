d4acf083cfbcbe8af141350e15310d1e17193fa7
malaythakur <malaythakur13@gmail.com>
2024-09-09 20:20:55 +0530

    feat: add client-side rendering and authentication dropdown menu to component
    - Added  directive to enable client-side rendering.
    - Integrated  hook and NextAuth methods (, , ) for authentication handling.
    - Imported and implemented dropdown menu components (, , etc.) for enhanced user interaction.

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+"use client"
+import React,{useEffect} from 'react'
 import Image from 'next/image'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 function Header() {
+
+  const {data} = useSession();
+
+  useEffect(()=>{
+    console.log(data);
+  },[data])
+
   return (
     <div className='p-5 shadow-sm flex justify-between'>
         <div className='flex items-center gap-8'>
             <Image src='/logo.svg' alt='logo'
-            width={50} height={50} />
+            width={50} height={50} priority/>
             <div className='md:flex items-center gap-6 hidden'>
                 <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Home</h2>
                 <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Services</h2>
@@ -17,7 +35,44 @@ function Header() {
         </div>
 
         <div>
-          <Button>Get Started</Button>
+
+          {data?.user?
+          
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Image src={data?.user?.image}
+                alt='user'
+                width={40}
+                height={40}
+                className='rounded-full'
+              />
+
+            </DropdownMenuTrigger>
+              
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  My Account
+                </DropdownMenuLabel>
+                
+              <DropdownMenuSeparator />
+                
+                <DropdownMenuItem>
+                  My Booking
+                </DropdownMenuItem>
+                
+                <DropdownMenuItem onClick={()=>signOut()}>
+                  Logout
+                </DropdownMenuItem>
+
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+          :
+          
+          <Button onClick={()=>signIn('descope')}>Login / Sign Up</Button>
+
+        
+        }
         </div>
     </div>
 
