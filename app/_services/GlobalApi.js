dc1dbf9511e1b16c5554aa01ab8f3e2c19d2ad1a
malaythakur <malaythakur13@gmail.com>
2024-09-12 01:58:20 +0530

    Implemented getBusinessByCategory function to query businesses by category name

--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -44,7 +44,33 @@ const getAllBusinessList=async()=>{
     return result;
 }
 
+const getBusinessByCategory=async(category)=>{
+    const query=gql`
+    query MyQuery {
+    businessLists(where: {category:
+     {name: "`+category+`"}}) {
+    about
+    address
+    category {
+      name
+    }
+    contactPerson
+    email
+    id
+    name
+    images {
+      url
+    }
+  }
+}
+    `
+    const result= await request(MASTER_URL,query)
+    return result;
+}
+
+
 export default{
     getCategory,
-    getAllBusinessList
+    getAllBusinessList,
+    getBusinessByCategory
 }
