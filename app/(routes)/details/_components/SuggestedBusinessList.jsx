21e589a8fa2d30f4220b4cb002e5c2285845bb5c
malaythakur <malaythakur13@gmail.com>
2024-09-13 20:19:27 +0530

    Enhance SuggestedBusinessList with API Integration and Styling
    - Integrated API call to fetch and display suggested businesses based on the current business category.
    - Implemented state management to handle the list of suggested businesses.
    - Added styling and layout adjustments for better presentation within the BusinessDetail page.

--- app/(routes)/details/_components/SuggestedBusinessList.jsx
+++ app/(routes)/details/_components/SuggestedBusinessList.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { NotebookPen } from 'lucide-react'
+import React, {useState, useEffect} from 'react'
+import GlobalApi from '@/app/_services/GlobalApi';
+import Image from 'next/image';
+import Link from 'next/link';
+    
+function SuggestedBusinessList({business}) {
 
-function SuggestedBusinessList() {
+    const [businessList, setBusinessList] = useState([]);
+
+  useEffect(() => {
+    business&&getBusinessList()
+  },[business]);
+
+  const getBusinessList=()=>{
+    GlobalApi.getBusinessByCategory(business?.category?.name)
+    .then(resp=>{
+      setBusinessList(resp?.businessLists);
+    })
+  }
+
   return (
-    <div>SuggestedBusinessList</div>
+    <div className='md:pl-10'>
+        <Button className="flex gap-2 w-full">
+            <NotebookPen/>
+            Book Appointment
+        </Button>
+        <div className='hidden md:block'>
+        <h2 className='font-bold text-lg mt-3 mb-3'>Similar Business</h2>
+        
+        <div>
+            {businessList&&businessList.map((business,index)=>(
+                <Link href={'/details/'+ business.id}className='flex gap-2 mb-4 
+                hover:border rounded-lg p-2
+                cursor-pointer hover:shadow-md border-primary'>
+                    <Image src={business?.images[0].url}
+                    alt={business.name}
+                    width={80}
+                    height={80}                
+                    className='rounded-lg object-cover'
+                    />
+
+                    <div>
+                        <h2 className='font-bold'>{business.name}</h2>
+                        <h2 className='text-primary'>{business.contactPerson}</h2>
+                        <h2 className='text-gray-400'>{business.address}</h2>
+                    
+                    </div> 
+                </Link>
+            ))}            
+        </div>
+        </div>
+    </div>
   )
 }
 
