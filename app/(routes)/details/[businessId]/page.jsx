d6b04fa57f8f8c7d4254b29ab90babb8ec880523
malaythakur <malaythakur13@gmail.com>
2024-09-13 20:16:48 +0530

    Refactor Layout of Business Detail Section for Improved Responsiveness
    - Adjusted grid layout to a 3-column structure for better alignment.
    - Modified column spans for BusinessDescription and SuggestedBusinessList components.
    - Changed the order of elements to enhance the layout on different screen sizes.

--- app/(routes)/details/[businessId]/page.jsx
+++ app/(routes)/details/[businessId]/page.jsx
@@ -40,12 +40,12 @@ function BusinessDetail({params}) {
     <div className='py-8 md:py-20 px-10 md:px-36'>
         <BusinessInfo business={business} />
 
-        <div className='grid grid-cols-4 mt-16'>
-            <div className='col-span-4 md:col-span-3'>
+        <div className='grid grid-cols-3 mt-16'>
+            <div className='col-span-3 md:col-span-2 order-last md:order-first'>
                 <BusinessDescription business={business}/>
             </div>
 
-            <div className='hidden md:block'>
+            <div className=''>
                 <SuggestedBusinessList business={business}/>
             </div>
         </div>
