395b06923526280502b2cd7de0dc295d25ec9a0c
malaythakur <malaythakur13@gmail.com>
2024-09-13 11:56:48 +0530

    Enhance BusinessDetail Component with Business Data and Additional Info Sections
    - Added API integration to fetch business details by ID.
    - Imported and integrated BusinessInfo, SuggestedBusinessList, and BusinessDescription components.
    - Added state management for business data.
    - Implemented useEffect hooks for fetching business data and checking user authentication.

--- app/(routes)/details/[businessId]/page.jsx
+++ app/(routes)/details/[businessId]/page.jsx
@@ -1,21 +1,56 @@
 "use client"
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import GlobalApi from '@/app/_services/GlobalApi';
+import React, { useEffect, useState  } from 'react'
 import { signIn } from 'next-auth/react';
-function BusinessDetail() {
+import BusinessInfo from '../_components/BusinessInfo';
+import SuggestedBusinessList from '../_components/SuggestedBusinessList';
+import BusinessDescription from '../_components/BusinessDescription';
+function BusinessDetail({params}) {
 
   const {data,status}=useSession();
-  
-   if(status=='loading'){
-        return <p>Loading...</p>
+  const [business,setBusiness]=useState([]);
+
+   useEffect(()=>{
+    params&&getbusinessById();
+   }, [params])
+
+   useEffect(()=>{
+        checkUserAuth(); 
+   })
+
+   const checkUserAuth=()=>{
+        if(status=='loading'){
+            return <p>Loading...</p>
+        }
+
+        if(status=='unauthenticated'){
+            signIn('descope');
+        }
    }
 
-   if(status=='unauthenticated'){
-        signIn('descope');
+   const getbusinessById=()=>{
+    GlobalApi.getBusinessById(params.businessId).then(resp=>{
+        setBusiness(resp.businessList);
+    })
    }
+
    
-   return status=='authenticated' &&(
-    <div>BusinessDetail</div>
+   return status=='authenticated' &&business&&(
+    <div className='py-8 md:py-20 px-10 md:px-36'>
+        <BusinessInfo business={business} />
+
+        <div className='grid grid-cols-4 mt-8'>
+            <div className='col-span-3'>
+                <BusinessDescription business={business}/>
+            </div>
+
+            <div className='hidden md:block'>
+                <SuggestedBusinessList business={business}/>
+            </div>
+        </div>
+        
+    </div>
    )
 
 }
