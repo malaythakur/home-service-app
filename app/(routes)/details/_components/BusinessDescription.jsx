45eadd7bcffc554fb07d555f81dc27621523e6ec
malaythakur <malaythakur13@gmail.com>
2024-09-13 12:50:17 +0530

    Enhance BusinessDescription Component with Detailed Info and Gallery
    - Updated BusinessDescription to display business details and description text.
    - Rendered each line of the description as a separate paragraph for better readability.
    - Added a gallery section to display business images in a responsive grid layout.

--- app/(routes)/details/_components/BusinessDescription.jsx
+++ app/(routes)/details/_components/BusinessDescription.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+function BusinessDescription({business}) {
+  return business?.name&&(
+    <div>
+        <h2 className='font-bold text-[25px]'>Description</h2>
+        <p className='mt-4 text-lg text-gray-600'>{business.about}</p>
+        {/* Render each line as a separate paragraph
 
-function BusinessDescription() {
-  return (
-    <div>BusinessDescription</div>
+            {business.about.split('\n').map((line, index) => (
+                <p key={index}>{line}</p>
+            ))} 
+         
+        */}
+
+        <h2 className='font-bold text-[25px] mt-8'>Gallary</h2>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5'>
+            {business?.images?.map((item,index)=>(
+                <Image src={item.url}
+                key={index}
+                alt="image"
+                width={700}
+                height={200}
+                primary
+                className="rounded-lg"
+                />
+            ))}
+        </div>
+    
+    </div>
   )
 }
 
