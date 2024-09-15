af7e1ff7111a76a652a68172365f2a5179a40110
malaythakur <malaythakur13@gmail.com>
2024-09-15 17:52:47 +0530

    Added My Booking Tab to Display Booking History
    - Created MyBooking component with tabs to toggle between Booked and Completed bookings.
    - Integrated BookingHistoryList component to display booking details based on user data.
    - Added functionality to fetch and filter user's booking history via GlobalApi.GetUserBookingHistory.
    - Used Tabs, TabsContent, TabsList, and TabsTrigger components for tab navigation.

---
+++ app/(routes)/mybooking/page.jsx
@@ -0,0 +1,53 @@
+"use client"
+import React, { useState, useEffect } from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import BookingHistoryList from './_component/BookingHistoryList'
+import GlobalApi from '@/app/_services/GlobalApi'
+import { useSession } from 'next-auth/react'
+
+function MyBooking() {
+
+    const {data} = useSession();
+    const [bookingHistory,setBookingHistory]=useState([]);
+
+    useEffect(()=>{
+        data&&GetUserBookingHistory();
+    },[data])
+     
+    const GetUserBookingHistory=()=>{
+        GlobalApi.GetUserBookingHistory(data.user.email).then(resp=>{
+            console.log(resp);
+            setBookingHistory(resp.bookings);
+        })
+    }
+
+    const filterData=(type)=>{
+        const result=bookingHistory.filter(item=>
+            type=="booked"?
+            new Date(item.date)>new Date()
+            :new Date(item.date)<new Date());
+
+            return result;
+    }
+    return (
+        <div className='my-10 mx-5 md:mx-36'>
+            <h2 className='font-bold text-[20px] my-2'>My Bookings</h2>
+            <Tabs defaultValue="booked" className="w-full">
+                <TabsList className="w-full justify-start ">
+                    <TabsTrigger value="booked">Booked</TabsTrigger>
+                    <TabsTrigger value="completed">Completed</TabsTrigger>
+                </TabsList>
+                <TabsContent value="booked">
+                    <BookingHistoryList bookingHistory={filterData('booked')}/>  
+                </TabsContent>
+                <TabsContent value="completed">
+                <BookingHistoryList bookingHistory={filterData('completed')}/>  
+
+                </TabsContent>
+            </Tabs>
+
+        </div>
+    )
+}
+
+export default MyBooking 
