1eb69bd1ee68437e1f5c87decedd7e77c51b9661
malaythakur <malaythakur13@gmail.com>
2024-09-15 17:42:10 +0530

    Added a Link component for navigation to the Home page

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
@@ -11,33 +11,33 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-
+import Link from 'next/link'
 
 function Header() {
 
-  const {data} = useSession();
+  const { data } = useSession();
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(data);
-  },[data])
+  }, [data])
 
   return (
     <div className='p-5 shadow-sm flex justify-between'>
-        <div className='flex items-center gap-8'>
-            <Image src='/logo.svg' alt='logo'
-            width={50} height={50} priority/>
-            <div className='md:flex items-center gap-6 hidden'>
-                <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Home</h2>
-                <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Services</h2>
-                <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>About Us</h2>
-            </div>
-
+      <div className='flex items-center gap-8'>
+        <Image src='/logo.svg' alt='logo'
+          width={50} height={50} priority />
+        <div className='md:flex items-center gap-6 hidden'>
+          <Link href={'/'} className='hover:scale-105 hover:text-primary cursor-pointer'>Home</Link>
+          <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>Services</h2>
+          <h2 className='hover:scale-105 hover:text-primary cursor-pointer'>About Us</h2>
         </div>
 
-        <div>
+      </div>
 
-          {data?.user?
-          
+      <div>
+
+        {data?.user ?
+
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Image src={data?.user?.image}
@@ -48,32 +48,32 @@ function Header() {
               />
 
             </DropdownMenuTrigger>
-              
-              <DropdownMenuContent>
-                <DropdownMenuLabel>
-                  My Account
-                </DropdownMenuLabel>
-                
-              <DropdownMenuSeparator />
-                
-                <DropdownMenuItem>
-                  My Booking
-                </DropdownMenuItem>
-                
-                <DropdownMenuItem onClick={()=>signOut()}>
-                  Logout
-                </DropdownMenuItem>
 
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                My Account
+              </DropdownMenuLabel>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem>
+                <Link href={'/mybooking'}>My Booking</Link>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem onClick={() => signOut()}>
+                Logout
+              </DropdownMenuItem>
+
+            </DropdownMenuContent>
+          </DropdownMenu>
 
           :
-          
-          <Button onClick={()=>signIn('descope')}>Login / Sign Up</Button>
 
-        
+          <Button onClick={() => signIn('descope')}>Login / Sign Up</Button>
+
+
         }
-        </div>
+      </div>
     </div>
 
   )
